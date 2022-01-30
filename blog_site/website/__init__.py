9188d328a3351d9a1aa22ad934caceafd7caf81d
Eric Sun <shengkai.sun@gmail.com>
2022-01-30 12:42:04 -0800

    error handling

--- blog_site/website/__init__.py
+++ blog_site/website/__init__.py
@@ -3,4 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from website.core.views import core
+from website.errors.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
