d09aabdb280ced0a186ee9b93fb982f0d01154a1
Eric Sun <shengkai.sun@gmail.com>
2022-01-30 14:28:49 -0800

    Setup DB and models

--- blog_site/website/__init__.py
+++ blog_site/website/__init__.py
@@ -1,6 +1,28 @@
 from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+################################
+#### Database setup ###
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///'+ os.path.join(basedir,'site_db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+################################
+# Login configs
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+################################
 
 from website.core.views import core
 from website.errors.handlers import error_pages
